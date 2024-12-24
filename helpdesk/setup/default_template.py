24abbcf5dc579c5d4c25994e98d02dcba53f948e
Sampada <sampadak@byteshores.com>
2024-12-24 11:52:08 +0000

    feat : added a check for default template.

--- helpdesk/setup/default_template.py
+++ helpdesk/setup/default_template.py
@@ -4,8 +4,12 @@ from helpdesk.consts import DEFAULT_TICKET_TEMPLATE
 
 
 def create_default_template():
-    doc = {
-        "doctype": "HD Ticket Template",
-        "template_name": DEFAULT_TICKET_TEMPLATE,
-    }
-    frappe.get_doc(doc).save()
+    existing_template = frappe.db.exists(
+        "HD Ticket Template", {"template_name": DEFAULT_TICKET_TEMPLATE}
+    )
+    if not existing_template:
+        doc = {
+            "doctype": "HD Ticket Template",
+            "template_name": DEFAULT_TICKET_TEMPLATE,
+        }
+        frappe.get_doc(doc).save()
